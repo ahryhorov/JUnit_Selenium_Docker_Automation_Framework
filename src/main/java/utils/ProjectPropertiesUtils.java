54651d05216912afbca3d5f119936373ec5d8f95
ahryhorov <ahryhorov@prounlimited.com>
2024-06-14 10:50:43 +0300

    - Fixing ProjectPropertiesUtils.java for Parallel Run

--- src/main/java/utils/ProjectPropertiesUtils.java
+++ src/main/java/utils/ProjectPropertiesUtils.java
@@ -13,34 +13,51 @@ public class ProjectPropertiesUtils {
 
     private static ProjectProperties projectProperties;
 
-    public static ProjectProperties getProjectProperties() {
-        if (instance == null) {
-            initialize();
-        }
-        return projectProperties;
-        //needed to exclude duplicate initializations
-    }
-
-    public static void initialize() {
-        properties = new Properties();
-        projectProperties = new ProjectProperties();
-        try {
-            is = ProjectPropertiesUtils.class.getClassLoader().getResourceAsStream(PROJECT_PROPERTIES_FILE);
-            //TODO FIX Properties for Parallel run
+    private static ThreadLocal<ProjectProperties> projectPropertiesThreadLocal = ThreadLocal.withInitial(() -> {
+        Properties properties = new Properties();
+        ProjectProperties projectProperties = new ProjectProperties();
+        try (InputStream is = ProjectPropertiesUtils.class.getClassLoader().getResourceAsStream(PROJECT_PROPERTIES_FILE)) {
             properties.load(is);
             projectProperties.setSelectedHub(properties.getProperty("selectRunHub"));
             projectProperties.setDefaultTimeout(properties.getProperty("defaultTimeout"));
-            //add other properties here...
+            // add other properties here...
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            if (is != null) {
-                try {
-                    is.close();
-                } catch (Exception e2) {
-                    e2.printStackTrace();
-                }
-            }
         }
+        return projectProperties;
+    });
+
+    public static ProjectProperties getProjectProperties() {
+        return projectPropertiesThreadLocal.get();
     }
+
+    //    public static ProjectProperties getProjectProperties() {
+//        if (instance == null) {
+//            initialize();
+//        }
+//        return projectProperties;
+//        //needed to exclude duplicate initializations
+//    }
+//
+//    public static void initialize() {
+//        properties = new Properties();
+//        projectProperties = new ProjectProperties();
+//        try {
+//            is = ProjectPropertiesUtils.class.getClassLoader().getResourceAsStream(PROJECT_PROPERTIES_FILE);
+//            properties.load(is);
+//            projectProperties.setSelectedHub(properties.getProperty("selectRunHub"));
+//            projectProperties.setDefaultTimeout(properties.getProperty("defaultTimeout"));
+//            //add other properties here...
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (is != null) {
+//                try {
+//                    is.close();
+//                } catch (Exception e2) {
+//                    e2.printStackTrace();
+//                }
+//            }
+//        }
+//    }
 }
